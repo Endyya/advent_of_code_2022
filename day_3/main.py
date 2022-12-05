568bf728e171d6c1464a3059a6733c18661e1041
Endya <endya@protonmail.com>
2022-12-05 10:34:41 +0100

    day 3 part 1 finished

--- day_3/main.py
+++ day_3/main.py
@@ -1,2 +1,25 @@
+import string
+
+
+def get_priority(filename):
+
+	with open(filename, 'r') as f:
+		for line in f:
+			line = line.replace('\n', '')
+			assert len(line) % 2 == 0
+			part1 = line[:len(line)//2]
+			part2 = line[len(line)//2:]
+
+			common_letter = set(part1) & set(part2)
+			priority = string.ascii_letters.index(list(common_letter)[0]) + 1
+
+			yield priority
+
 if __name__ == '__main__':
-	pass
+	
+	#part 1
+	count = 0
+	for priority in get_priority('input'):
+		count += priority
+
+	print('The sum of priorities of duplicates item is :', count)
