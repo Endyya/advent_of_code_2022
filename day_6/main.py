01a7f3c99c2a5ec55c6171614c5e9ef2e7b970b8
Endya <endya@protonmail.com>
2022-12-06 17:06:42 +0100

    day 6 finished

--- day_6/main.py
+++ day_6/main.py
@@ -1,2 +1,21 @@
 if __name__ == '__main__':
-	pass
+	with open('input', 'r') as f:
+		text = f.readline()
+
+	# part 1
+	i = 4
+	window = text[:4]
+	while len(set(window)) < 4:
+		i += 1
+		window = text[(i-4):i]
+
+	print(f'The first start-of-packet marker is detected at {i} character')
+
+	# part 1
+	i = 14
+	window = text[:14]
+	while len(set(window)) < 14:
+		i += 1
+		window = text[(i-14):i]
+
+	print(f'The first start-of-packet marker is detected at {i} character')
