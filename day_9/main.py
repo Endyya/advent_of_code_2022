ba14eb45d35ee3e9f7e98be323af2a480156f2b6
Endya <endya@protonmail.com>
2022-12-14 11:38:57 +0100

    day 9 part 2 finished

--- day_9/main.py
+++ day_9/main.py
@@ -1,5 +1,38 @@
+def update(pos_head, pos_tail):
+	# update the tail
 
-def move(direction, pos_head, pos_tail):
+	rel_pos = (0, 0)
+	if pos_head[0] == pos_tail[0] + 2:
+		rel_pos = (
+			rel_pos[0] - 1,
+			rel_pos[1] + 0)
+	if pos_head[0] == pos_tail[0] - 2:
+		rel_pos = rel_pos = (
+			rel_pos[0] + 1,
+			rel_pos[1] + 0)
+	if pos_head[1] == pos_tail[1] + 2:
+		rel_pos = (
+			rel_pos[0] - 0,
+			rel_pos[1] - 1)
+	if pos_head[1] == pos_tail[1] - 2:
+		rel_pos = (
+			rel_pos[0] - 0,
+			rel_pos[1] + 1)
+	if max(
+		abs(pos_head[0] - pos_tail[0]),
+		abs(pos_head[1] - pos_tail[1])) < 2:
+
+		rel_pos = (
+			pos_tail[0] - pos_head[0],
+			pos_tail[1] - pos_head[1])
+
+	pos_tail = (
+		pos_head[0] + rel_pos[0],
+		pos_head[1] + rel_pos[1]
+		)
+	return pos_tail
+
+def move(direction, pos_head):
 
 	# update the head
 	if direction == 'U':
@@ -18,37 +51,26 @@ def move(direction, pos_head, pos_tail):
 		pos_head[1] + rel_pos[1]
 		)
 
-	# update the tail
-	if new_pos_head[0] == pos_tail[0] + 2:
-		rel_pos = (-1, 0)
-	elif new_pos_head[0] == pos_tail[0] - 2:
-		rel_pos = (1, 0)
-	elif new_pos_head[1] == pos_tail[1] + 2:
-		rel_pos = (0, -1)
-	elif new_pos_head[1] == pos_tail[1] - 2:
-		rel_pos = (0, 1)
-	else:
-		rel_pos = (
-			pos_tail[0] - new_pos_head[0],
-			pos_tail[1] - new_pos_head[1])
-
-	new_pos_tail = (
-		new_pos_head[0] + rel_pos[0],
-		new_pos_head[1] + rel_pos[1]
-		)
-
-	return (new_pos_head, new_pos_tail)
+	return new_pos_head
 
 def main(part = 1, test = False):
-	if test:
+	if test and part == 1:
 		filename = 'inputtest'
+	elif test and part == 2:
+		filename = 'inputtestp2'
 	else:
 		filename = 'input'
 
-	pos_head = (0, 0)
-	pos_tail = (0, 0)
+	if part == 1:
+		rope_size = 2
+	elif part == 2:
+		rope_size = 10
+	else:
+		raise Exception
 
-	history_tail = set([pos_tail])
+	pos_knots = [(0, 0)] * rope_size
+
+	history_tail = set([pos_knots[-1]])
 
 	with open(filename, 'r') as f:
 		line = f.readline()
@@ -57,8 +79,11 @@ def main(part = 1, test = False):
 			direction, number = line.split(' ')
 			number = int(number)
 			for _ in range(number):
-				pos_head, pos_tail = move(direction, pos_head, pos_tail)
-				history_tail.update([pos_tail])
+				pos_knots[0] = move(direction, pos_knots[0])
+				for i in range(rope_size - 1):
+					pos_knots[i + 1] = update(pos_knots[i], pos_knots[i + 1])
+
+				history_tail.update([pos_knots[-1]])
 
 			line = f.readline()
 
@@ -70,4 +95,4 @@ if __name__ == '__main__':
 	main(part = 1, test = False)
 
 	#part 2
-	main(part = 2, test = True)
+	main(part = 2, test = False)
